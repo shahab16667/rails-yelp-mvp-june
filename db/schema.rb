55870a10626e4f949cae67f2f33ef2c6ebdb1750
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-23 20:14:44 +1000

    model Review has been added

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_100735) do
+ActiveRecord::Schema.define(version: 2020_06_23_101355) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.integer "phone_number"
     t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
