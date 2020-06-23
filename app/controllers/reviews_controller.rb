b1b99e78a419ae14a90b93d9c170056a00d0c546
Shahab Jahangiri <shahab.j3@gmail.com>
2020-06-23 20:32:47 +1000

    added some routes for restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+
+    def new 
+      @review = Review.create!
+    end
+
+
+    private 
+
+    def find_restaurant 
+        @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+
+    def review_params 
+        params.require(:review).permit(:content)
+    end
+
 end
